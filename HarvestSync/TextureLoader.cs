0f1088ab488078d0681225396bad16527783e289
ChippedChap <mikiel.theg@gmail.com>
2019-02-16 17:55:11 -0500

    Forgot StaticConstructorOnStartup

--- HarvestSync/TextureLoader.cs
+++ HarvestSync/TextureLoader.cs
@@ -3,6 +3,7 @@ using Verse;
 
 namespace HarvestSync
 {
+	[StaticConstructorOnStartup]
 	class TextureLoader
 	{
 		public static Texture2D setHarvestProportionIcon = ContentFinder<Texture2D>.Get("setHarvestProportion");
