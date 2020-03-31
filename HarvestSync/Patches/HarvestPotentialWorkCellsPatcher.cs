f0cd51b630b9ff9d401a1c14242f593be6d7bd14
ChippedChap <mikiel.theg@gmail.com>
2020-03-31 16:33:29 -0400

    Update patch

--- HarvestSync/Patches/HarvestPotentialWorkCellsPatcher.cs
+++ HarvestSync/Patches/HarvestPotentialWorkCellsPatcher.cs
@@ -1,15 +1,17 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System;
+using Verse;
 
 namespace HarvestSync
 {
-	[HarmonyPatch(typeof(WorkGiver_GrowerHarvest))]
+	[HarmonyPatch(typeof(WorkGiver_Grower))]
 	[HarmonyPatch("ExtraRequirements")]
 	class HarvestPotentialWorkCellsPatcher
 	{
-		static void Postfix(ref bool __result, IPlantToGrowSettable settable)
+		static void Postfix(ref bool __result, ref WorkGiver_Grower __instance, IPlantToGrowSettable settable)
 		{
-			if (settable is Zone_Growing zone)
+			if (__instance is WorkGiver_GrowerHarvest && settable is Zone_Growing zone)
 			{
 				__result = zone.Map.GetComponent<HarvestManager_MapComponent>().CanHarvestZone(zone);
 			}
